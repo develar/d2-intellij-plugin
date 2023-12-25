3f61cba6e0cdaf48fa4e93cccf9c389c0009017d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-12-25 08:51:15 +0100

    D2SpellCheckerStrategy - support string literals

--- src/main/kotlin/org/jetbrains/plugins/d2/editor/D2SpellCheckerStrategy.kt
+++ src/main/kotlin/org/jetbrains/plugins/d2/editor/D2SpellCheckerStrategy.kt
@@ -1,17 +1,23 @@
 package org.jetbrains.plugins.d2.editor
 
+import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 import com.intellij.spellchecker.inspections.CommentSplitter
+import com.intellij.spellchecker.inspections.PlainTextSplitter
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
 import com.intellij.spellchecker.tokenizer.TokenConsumer
 import com.intellij.spellchecker.tokenizer.Tokenizer
+import org.jetbrains.plugins.d2.lang.D2ElementTypes
 
 private class D2SpellCheckerStrategy : SpellcheckingStrategy() {
   override fun getTokenizer(element: PsiElement): Tokenizer<*> {
-    return when (element) {
-      is PsiComment -> D2CommentTokenizer
+    return when {
+      element is PsiComment -> D2CommentTokenizer
+      element.elementType == D2ElementTypes.STRING -> D2StringLiteralTokenizer
       else -> EMPTY_TOKENIZER
     }
   }
@@ -32,4 +38,87 @@ private object D2CommentTokenizer : Tokenizer<PsiComment>() {
       /* splitter = */ CommentSplitter.getInstance()
     )
   }
+}
+
+private val STRING_FRAGMENTS = Key<List<Pair<TextRange, String>>>("D2 string fragments")
+private const val escapeTable = "\"\"\\\\//b\bf\u000cn\nr\rt\t"
+
+private object D2StringLiteralTokenizer : Tokenizer<PsiElement>() {
+  override fun tokenize(element: PsiElement, consumer: TokenConsumer) {
+    val textSplitter = PlainTextSplitter.getInstance()
+    if (element.textContains('\\')) {
+      val fragments = getTextFragments(element)
+      for (fragment in fragments) {
+        val fragmentRange = fragment.first
+        val escaped = fragment.second
+        if (escaped.length == fragmentRange.length && !escaped.startsWith("\\")) {
+          consumer.consumeToken(element, escaped, false, fragmentRange.startOffset, TextRange.allOf(escaped), textSplitter)
+        }
+      }
+    } else {
+      consumer.consumeToken(element, textSplitter)
+    }
+  }
+}
+
+private fun getTextFragments(literal: PsiElement): List<Pair<TextRange, String>> {
+  literal.getUserData(STRING_FRAGMENTS)?.let {
+    return it
+  }
+
+  val result = ArrayList<Pair<TextRange, String>>()
+  val text = literal.text
+  val length = text.length
+  var pos = 1
+  var unescapedSequenceStart = 1
+  while (pos < length) {
+    if (text.get(pos) == '\\') {
+      if (unescapedSequenceStart != pos) {
+        result.add(Pair(TextRange(unescapedSequenceStart, pos), text.substring(unescapedSequenceStart, pos)))
+      }
+      if (pos == length - 1) {
+        result.add(Pair(TextRange(pos, pos + 1), "\\"))
+        break
+      }
+      when (val next = text.get(pos + 1)) {
+        '"', '\\', '/', 'b', 'f', 'n', 'r', 't' -> {
+          val index = escapeTable.indexOf(next)
+          result.add(Pair(TextRange(pos, pos + 2), escapeTable.substring(index + 1, index + 2)))
+          pos += 2
+        }
+
+        'u' -> {
+          var i = pos + 2
+          while (i < pos + 6) {
+            if (i == length || !StringUtil.isHexDigit(text.get(i))) {
+              break
+            }
+            i++
+          }
+          result.add(Pair(TextRange(pos, i), text.substring(pos, i)))
+          pos = i
+        }
+
+        'x' -> {
+          result.add(Pair(TextRange(pos, pos + 2), text.substring(pos, pos + 2)))
+          pos += 2
+        }
+
+        else -> {
+          result.add(Pair(TextRange(pos, pos + 2), text.substring(pos, pos + 2)))
+          pos += 2
+        }
+      }
+      unescapedSequenceStart = pos
+    } else {
+      pos++
+    }
+  }
+  val contentEnd = if (text.get(0) == text.get(length - 1)) length - 1 else length
+  if (unescapedSequenceStart < contentEnd) {
+    result.add(Pair(TextRange(unescapedSequenceStart, contentEnd), text.substring(unescapedSequenceStart, contentEnd)))
+  }
+
+  literal.putUserData(STRING_FRAGMENTS, result)
+  return result
 }
