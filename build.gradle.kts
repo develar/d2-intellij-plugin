fe181f41a05cac52f08125b809b1fe18835c3e7e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-01-24 06:45:53 +0100

    chore: update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ val channel: String = properties("pluginVersion").split('-').getOrElse(1) { "def
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm") version "1.9.22"
-  id("org.jetbrains.intellij") version "1.16.1"
+  id("org.jetbrains.intellij") version "1.17.0"
   id("org.jetbrains.changelog") version "2.2.0"
-  id("org.jetbrains.qodana") version "2023.3.7"
+  id("org.jetbrains.qodana") version "2023.3.1"
   id("org.jetbrains.kotlinx.kover") version "0.7.5"
 
   id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
